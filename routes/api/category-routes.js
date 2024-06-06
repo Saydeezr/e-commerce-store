dd397ebf272e2e8cfef8bb7f9f086e8315c7cb1e
Saydeezr <saydeezr@gmail.com>
2024-06-06 11:28:38 -0400

    worked on route to update a category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,13 +41,26 @@ router.post('/', async (req, res) => {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (error) { 
-    res.status(400).json(err);
+    res.status(400).json(error);
   }
 });
 
  // update a category by its `id` value
-router.put('/:id', (req, res) => {
- 
+router.put('/:id', async (req, res) => {
+ try {
+  const categoryData = await Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+   if(!categoryData){
+     res.status(404).json({ message: 'Category not found' });
+     return;
+  } res.status(200).json(categoryData);
+  
+ } catch (error) {
+  res.status(500).json(error);
+ }
 });
 
 router.delete('/:id', (req, res) => {
