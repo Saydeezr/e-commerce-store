3973fac49e37bad2190ac1e1ffe94a8919a0aaeb
Saydeezr <saydeezr@gmail.com>
2024-06-12 19:48:30 -0400

    finished route to delete one product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -119,8 +119,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+// delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+   try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!productData) {
+      res.status(404).json({message: 'Product not found'});
+      return;
+    }
+
+    res.status(200).json(productData);
+   } catch( error) {
+    res.status(500).json(error);
+   }
 });
 
 module.exports = router;
