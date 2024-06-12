434079ad013d97a9362c0c46dd4f580c0a5c05ce
Saydeezr <saydeezr@gmail.com>
2024-06-12 19:15:10 -0400

    filled out route function to find all products and one product by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,42 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        through: 'productTag',
+        as: 'tags'
+      }
+    ]})
+    res.json(productData);
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try { // find a single product by its `id`
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }]
+    }, {
+      include: [{ model: Tag }],
+    });
+    if (!productData){
+      res.status(404).json({message: 'Product not found'});
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // create new product
